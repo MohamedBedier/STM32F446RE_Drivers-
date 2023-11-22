cc2415b2cc9b6b7efa74d1fc365aae0705bf89c7
Mohamed_Bedier <113776299+MohamedBedier@users.noreply.github.com>
2023-11-22 12:36:51 +0200

    SCB Driver v1.0

---
+++ 1-MCAL/5-SCB/Inc/SCB_Private.h
@@ -0,0 +1,28 @@
+/*************************************************************/
+/*************************************************************/
+/**                                                         **/
+/** LayerArchitect: MCAL                                    **/
+/** File Name : SCB_private.h                               **/
+/** Auther    : MOHAMED BEDIER MOHAMED                      **/
+/** Verision :  1.00                                        **/
+/**                                                         **/
+/*************************************************************/
+/*************************************************************/
+
+#ifndef SCB_PRIVATE_H_
+#define SCB_PRIVATE_H_
+
+
+#define  VECTKEY_VALUE       0x05FA
+#define  VECTKEY_START_BIT   16u
+#define  PRIGROUP_START_BIT  8u
+#define  FAULT_MASK_BITS     7u
+#define  FAULT_ENA_START_BITS     16u
+
+#define  FAULT_PENDING_START_BITS    12u
+
+#define  NUM_SYS_EXCEPTIONS   10u
+
+
+
+#endif /* SCB_PRIVATE_H_ */
